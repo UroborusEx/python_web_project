70ec5a52676f563adb7fee5628810c4bbcd425d7
UroborusEx <bmv2096@yandex.ru>
2016-10-07 01:53:35 +0300

    Stepic 2 complete

--- init.sh
+++ init.sh
@@ -5,4 +5,5 @@ sudo ln  -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn -c /etc/gunicorn.d/test restart
+sudo /etc/init.d/gunicorn start
+//gunicorn -c /etc/gunicorn.d/test hello:app
